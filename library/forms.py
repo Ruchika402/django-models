0b4932a8a40d1807c93de0ca7319726ae2dfc0d8
Ruchika Adak <adakruchika5@gmail.com>
2026-05-12 20:16:25 +0530

    update the library/forms.py

--- library/forms.py
+++ library/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Book
+from .models import Book,Review
 
 class BookForm(forms.ModelForm):
     class Meta:
@@ -7,4 +7,15 @@ class BookForm(forms.ModelForm):
         fields = ['title','author','pub_year']
         widgets = {
             'pub_year':forms.NumberInput(attrs={'placeholder':'e.g., 1997'}),
+
         }
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['reviewer_name', 'rating', 'comment']
+        widgets = {
+            'reviewer_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'rating': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }   
